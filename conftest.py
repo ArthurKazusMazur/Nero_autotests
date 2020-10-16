b3bfc3fd0e9df1c1952c7fff86e3340e69bc2d47
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-16 17:07:59 +0300

    arrange reporting commands are added

--- conftest.py
+++ conftest.py
@@ -14,19 +14,23 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
+
     if browser_name == "chrome":
         print("\n\nStarting GOOGLE Chrome browser for test...")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-
         options.add_argument('headless')  # эта опция позволяет запускать браузер "в безголовом" режиме
-
         browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+        browser.delete_all_cookies()
+
     elif browser_name == "firefox":
         print("\n\nStarting Mazila Firefox browser for test...")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
+        browser.delete_all_cookies()
     else:
         raise pytest.UsageError("--browser_name should be 'chrome' or 'firefox'")
 
